67557392e9a2d9ffcf8cb104c5a1ac1a30afa7e8
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2021-12-21 23:55:05 +0530

    feat: basic JS

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const dob = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check");
+const output = document.querySelector("#output-box");
 
 checkButton.addEventListener('click',function getValues(){
-    console.log(dob.value,luckyNumber.value);
-})
+    const dateOfBirth = dob.value;
+    const dob2 = dateOfBirth.replaceAll("-","");
+    let sum = sumBirthDate(dob2);
+    checkBirthDate(sum,luckyNumber.value);
+});
+
+function checkBirthDate(sum,luckyNumber){
+    if(sum%luckyNumber == 0){
+        output.innerText = "Your Birthday is lucky 🚀";
+    }else{
+        output.innerText = "Your Birthday is not lucky 😞";
+    }
+}
+
+function sumBirthDate(dob2){
+    let sum = 0;
+    for(let i=0;i<dob2.length;i++){
+        sum += Number(dob2.charAt(i));
+    }
+    return sum;
+}
